8a9ffd2a861b8b05aa4d14c79c147901cdae4eb4
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-03-01 19:17:38 +0530

    Update bookSlice to work better

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,23 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+import booksItems from '../../booksItems';
 
-const initialState = {
-  books: [],
-};
-
+const initialState = booksItems;
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
-      const bookId = action.payload;
-      state.books.concat(bookId);
+      const newBook = {
+        id: nanoid(),
+        title: action.payload.title,
+        author: action.payload.author,
+      };
+      state.push(newBook);
     },
-    removeBook: (state, action) => {
-      const bookId = action.payload;
-      state.books.filter((book) => book.item_id !== bookId);
-    },
+    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
   },
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
-export default booksSlice.actions;
+export default booksSlice.reducer;
