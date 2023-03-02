581968187b6fc36fa5a797582cd5905003712597
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-03-02 19:48:44 +0530

    Refactore the code to send to the API

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
+import { v4 as uuidv4 } from 'uuid';
+import { booksActions, postBook } from '../redux/books/booksSlice';
 
 const Form = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+
   const onSetTitle = (event) => {
     setTitle(event.target.value);
   };
+
   const onSetAuthor = (event) => {
     setAuthor(event.target.value);
   };
 
   const onFormSubmit = (e) => {
     e.preventDefault();
-    dispatch(
-      addBook({
-        title,
-        author,
-      }),
-    );
+    if (!title.trim() || !author.trim()) return;
+
+    const bookData = {
+      item_id: uuidv4(),
+      title,
+      author,
+      category: 'Fiction',
+    };
+    dispatch(booksActions.addBook(bookData));
+    dispatch(postBook(bookData));
 
     setTitle('');
     setAuthor('');
