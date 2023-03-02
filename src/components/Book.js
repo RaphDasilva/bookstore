f6567b0d2d65a3fbf5b287d44ab7de94b5799a49
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-03-02 19:47:06 +0530

    Update index.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import { booksActions, deleteBook } from '../redux/books/booksSlice';
 
 const Book = (props) => {
   const dispatch = useDispatch();
   const { id, title, author } = props;
+
+  const removeBookHandler = (event) => {
+    const { id } = event.target.dataset;
+    dispatch(booksActions.removeBook(id));
+    dispatch(deleteBook(id));
+  };
   return (
     <div className="oneBook">
       <h2>{title}</h2>
-      <p>{author}</p>
-      <button type="button" className="bookButton" onClick={() => dispatch(removeBook(id))}>Remove</button>
+      <span>
+        {' '}
+        By
+        {' '}
+        {author}
+      </span>
+      <button type="button" data-id={id} className="bookButton" onClick={removeBookHandler}>Remove</button>
     </div>
   );
 };
