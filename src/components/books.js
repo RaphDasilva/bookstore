d0e095c971bbbc4144cd57f2be92e7102c3d569a
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-03-02 19:47:56 +0530

    Add extrareducers and API calls

--- src/components/books.js
+++ src/components/books.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
+import { getBooks } from '../redux/books/booksSlice';
 
 const Books = () => {
-  const storedBooks = useSelector((store) => store.books);
+  const { books, isLoading } = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+  let outPut;
+  if (isLoading) outPut = <h3>Loading...</h3>;
+
+  if (books?.length > 0) {
+    outPut = books.map((book) => (
+      <Book
+        key={book.item_id}
+        id={book.item_id}
+        title={book.title}
+        author={book.author}
+      />
+    ));
+  }
+
+  if (!isLoading && books?.length === 0) {
+    outPut = <h3>No books to display...</h3>;
+  }
+
   return (
     <>
       <div className="allBooks">
-        {
-          storedBooks.map((book) => (
-            <Book key={book.id} id={book.id} title={book.title} author={book.author} />
-          ))
-        }
+        {outPut}
       </div>
       <Form />
     </>
