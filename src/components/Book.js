956fbf1b6034e8811cf059c959752fc1ec34e526
Raphael Okolo <okolonnaemeka2017@gmail.com>
2023-03-03 17:31:03 +0530

    Style book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { booksActions, deleteBook } from '../redux/books/booksSlice';
+import style from '../style/books.module.css';
 
 const Book = (props) => {
   const dispatch = useDispatch();
@@ -13,15 +14,36 @@ const Book = (props) => {
     dispatch(deleteBook(id));
   };
   return (
-    <div className="oneBook">
-      <h2>{title}</h2>
-      <span>
-        {' '}
-        By
-        {' '}
-        {author}
-      </span>
-      <button type="button" data-id={id} className="bookButton" onClick={removeBookHandler}>Remove</button>
+    <div className={style.book}>
+      <div>
+        <div className={style.bookInfo}>
+          <div className={style.catss}>Action</div>
+          <div className={style.title}>{title}</div>
+          <div className={style.author}>{author}</div>
+        </div>
+        <div className={style.bookAction}>
+          <div>Comment</div>
+          <div className={style.divBar} />
+          <button className={style.removeButton} type="button" data-id={id} onClick={removeBookHandler}>Remove</button>
+          <div className={style.divBar} />
+          <div>Edith</div>
+        </div>
+
+      </div>
+      <div className={style.others}>
+        <div className={style.Oval} />
+        <div>
+          <div className={style.percentComplete}>64%</div>
+          <div className={style.completed}>Completed</div>
+        </div>
+      </div>
+      <div className={style.divBar2} />
+      <div className={style.chapterUpdate}>
+        <div className={style.currentChapter}>Current Chapter</div>
+        <div className={style.currentLesson}>Chapter 17</div>
+        <div className={style.updateProgress}>UPDATE PROGRESS</div>
+      </div>
+
     </div>
   );
 };
